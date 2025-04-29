2a3a47cd1dee2c255626042cd2cef119b1e83f19
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2025-04-29 22:16:00 +0300

    Add project files.

---
+++ IdentityChatEmail/Context/EmailContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityChatEmail.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatEmail.Context
+{
+    public class EmailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CIHAN\\MSSQLSERVER01; initial Catalog= EmailChatDb; integrated security=true;trust server certificate=true");
+
+        }
+    }
+}
