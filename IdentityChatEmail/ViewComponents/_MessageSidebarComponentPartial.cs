cb41e609371aa83f48a71cc827ba29edaa069297
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2025-05-15 12:28:44 +0300

    inbox, Profile completed

--- IdentityChatEmail/ViewComponents/_MessageSidebarComponentPartial.cs
+++ IdentityChatEmail/ViewComponents/_MessageSidebarComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityChatEmail.Context;
+using IdentityChatEmail.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityChatEmail.ViewComponents
 {
-    public class _MessageSidebarComponentPartial:ViewComponent
+    public class _MessageSidebarComponentPartial : ViewComponent
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly EmailContext _emailContext;
+
+        public _MessageSidebarComponentPartial(UserManager<AppUser> userManager, EmailContext emailContext)
+        {
+            _userManager = userManager;
+            _emailContext = emailContext;
+        }
+
         public IViewComponentResult Invoke()
         {
+            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+
+            ViewBag.ReceiveMessage = _emailContext.Messages.Count(x => x.ReceiverEmail == user.Email && x.IsRead == false);
+
+            ViewBag.SendMessage = _emailContext.Messages.Count(x => x.SenderEmail == user.Email);
+
             return View();
         }
     }
